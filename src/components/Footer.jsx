85342688a9bbb174adbefbe05b547aa54c0bed09
kai3kai2 <onlyyou000u@gmail.com>
2026-06-10 17:15:35 +0800

    Add visitor counter to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,13 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>🏝 濟州島 5天4夜自駕遊 - 2026</p>
+      <div style={{ marginTop: '8px' }}>
+        <img
+          src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fkai3kai2.github.io%2Fjeju-trip-2025%2F&count_bg=%232C7873&title_bg=%23888888&title=views&edge_flat=true"
+          alt="visitor count"
+          style={{ height: '18px' }}
+        />
+      </div>
     </footer>
   )
 }
